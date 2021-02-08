c13f459dc0b38886cb8c0406e064a5938316efe7
syogo-user <s.photo.trumpet.005@gmail.com>
2021-02-09 00:12:24 +0900

    Initial Commit

---
+++ YoutubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by 小野寺祥吾 on 2021/02/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
